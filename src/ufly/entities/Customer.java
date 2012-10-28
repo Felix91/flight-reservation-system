85c84b55bf74ffe5d3c34e2d6b26c48ec8353712
Joel <Joel@192.168.123.29>
2012-10-28 15:17:16 -0700

    Added modifiers for customer class

--- src/ufly/entities/Customer.java
+++ src/ufly/entities/Customer.java
@@ -1,9 +1,13 @@
 package ufly.entities;
 
+import java.util.Iterator;
 import java.util.Vector;
 
+import javax.jdo.PersistenceManager;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
+
+import com.google.appengine.api.datastore.Key;
 
 @PersistenceCapable
 public class Customer extends User {
@@ -16,6 +20,129 @@ public class Customer extends User {
 		this.lastName = lastName;
 		this.loyaltyPoints = 0; // Every Customer starts with 0 loyalty points
 	}
+	/*------------MODIFIERS---------------*/
+	/**
+	 * @param newFirstName	: new first name to update to
+	 * @param key : key that matches object on datastore
+	 * @return
+	 */
+	public void changeFirstName(String newFirstName,Key key)
+	{
+		PersistenceManager pm= PMF.get().getPersistenceManager();
+		//TODO: Implement a check to see this operation is legal
+		try
+		{
+			Customer c= pm.getObjectById(Customer.class, key);
+			c.firstName=newFirstName;
+		
+		}finally
+		{
+			pm.close();
+		}
+	}
+	
+	/**
+	 * @param newLastName	: new last name to update to
+	 * @param key : key that matches object on datastore
+	 * @return
+	 */
+	public void changeLastName(String newLastName,Key key)
+	{
+		PersistenceManager pm= PMF.get().getPersistenceManager();
+		//TODO: Implement a check to see this operation is legal
+		try
+		{
+			Customer c= pm.getObjectById(Customer.class, key);
+			c.lastName=newLastName;
+		
+		}finally
+		{
+			pm.close();
+		}
+	}
+	
+	/**
+	 * @param loyaltypoints	: add loyalty points
+	 * @param key : key that matches object on datastore
+	 * @return
+	 */
+	public void addLoyaltyPoints(int loyaltypoints,Key key)
+	{
+		PersistenceManager pm= PMF.get().getPersistenceManager();
+
+		try
+		{
+			Customer c= pm.getObjectById(Customer.class, key);
+			c.loyaltyPoints+=loyaltypoints;
+		
+		}finally
+		{
+			pm.close();
+		}
+	}
+	
+	/**
+	 * @param loyalpoints	: use loyalty points
+	 * @param key : key that matches object on datastore
+	 * @return
+	 */
+	public void useLoyaltyPoints (int loyaltypoints,Key key){
+		PersistenceManager pm= PMF.get().getPersistenceManager();
+		try
+		{
+			Customer c= pm.getObjectById(Customer.class, key);
+			c.loyaltyPoints-=loyaltypoints;
+		
+		}finally
+		{
+			pm.close();
+		}
+	}
+	
+	/**
+	 * @param fb	: add fb into flightbooking vector
+	 * @param key : key that matches object on datastore
+	 * @return
+	 */
+	public void addBooking(FlightBooking fb,Key key)
+	{
+		PersistenceManager pm= PMF.get().getPersistenceManager();
+		try
+		{
+			Customer c= pm.getObjectById(Customer.class, key);
+			c.flightBookings.add(fb);
+		}finally
+		{
+			pm.close();
+		}
+	}
+	
+	/**
+	 * @param fb	: remove fb from flightbooking vector
+	 * @param key : key that matches object on datastore
+	 * @return
+	 */
+	public void removeBooking(FlightBooking fb, Key key)
+	{
+		PersistenceManager pm= PMF.get().getPersistenceManager();
+
+		try
+		{	
+			Iterator<FlightBooking> itr=flightBookings.iterator();
+			Customer c= pm.getObjectById(Customer.class, key);
+			
+			while(itr.hasNext()){
+				FlightBooking cur_fb = itr.next();
+				if(cur_fb.getConfirmationNumber().equals(fb.getConfirmationNumber()))
+					c.flightBookings.remove(cur_fb);//since a flightbooking is unique this works
+			}
+		}finally
+		{
+			pm.close();
+		}
+	}
+	
+	
 	/*------------ACCESSORS--------------*/
 	/**
 	 * @return customer first name
