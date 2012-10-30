34818e401ca89a46a61c6bd7fb5573a617905fe2
Joel Vandergriendt <vandergriendtjoel@gmail.com>
2012-10-30 16:05:03 -0700

    add a superclass for all servlets containing common code

---
+++ src/ufly/frs/UflyServlet.java
@@ -0,0 +1,23 @@
+package ufly.frs;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpSession;
+
+import ufly.entities.Customer;
+import ufly.entities.User;
+
+@SuppressWarnings("serial")
+public class UflyServlet extends HttpServlet {
+	public User getLoggedInUser(HttpSession s)
+	{
+		User localUser =null;
+		String email = (String) s.getAttribute("loggedInUser");
+		localUser = Customer.getCustomer(email);
+		//add check for admin if localuser is null
+		return localUser;			
+	}
+	public void login(String email,HttpSession s)
+	{
+		s.setAttribute("loggedInUser", email);	
+	}
+}
