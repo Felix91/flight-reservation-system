7bb6f715ef81ef1a1bd19d01532406f0df6fc781
Joel Vandergriendt <vandergriendtjoel@gmail.com>
2012-11-02 21:08:06 -0700

    dynamically print whether user logged in or not

--- src/ufly/frs/Includer.java
+++ src/ufly/frs/Includer.java
@@ -7,19 +7,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ufly.entities.User;
+
 @SuppressWarnings("serial")
-public class Includer extends HttpServlet {
+public class Includer extends UflyServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 	throws ServletException,IOException
 	{
 		String pageToInclude= getServletConfig().getInitParameter("page");
-		if(pageToInclude.equals("footer"))
+		if(pageToInclude.equals("footer") )
 		{
 			req.getRequestDispatcher("foot.jsp")
 				.include(req,resp);
-		}else if (pageToInclude.equals("header"))
+		}else if (pageToInclude.equals("header") )
 		{
 			req.getRequestDispatcher("head.jsp")
+				.include(req,resp);
+		}else if ( pageToInclude.equals("navbar") )
+		{
+			User u = getLoggedInUser(req.getSession());
+			if ( u != null)
+			{
+				req.setAttribute("userEmailAddress", u.getEmailAddr());
+			}
+			req.getRequestDispatcher("/navBar.jsp")
 				.include(req,resp);
 		}
 			
