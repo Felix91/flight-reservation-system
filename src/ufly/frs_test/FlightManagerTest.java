ee6a1c91883560480079275d02223817295b0da6
Hanks <Hanks@206.87.131.30>
2012-11-04 17:57:10 -0800

    Completed FlightManager Test

---
+++ src/ufly/frs_test/FlightManagerTest.java
@@ -0,0 +1,46 @@
+package ufly.frs_test;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ufly.entities.Airport;
+import ufly.entities.PMF;
+import ufly.entities.User;
+import ufly.entities.FlightManager;
+
+@SuppressWarnings("serial")
+public class FlightManagerTest extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		req.getRequestDispatcher("FlightManagerTest.jsp").forward(req, resp);
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException,ServletException
+	{
+		String email = req.getParameter("emailAddr");
+		String password = req.getParameter("password");
+		
+		FlightManager newFlightManager = new FlightManager(email, password); 
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		try {
+            pm.makePersistent(newFlightManager);
+            
+        } finally {
+            pm.close();
+        }
+		
+		//resp.setContentType("text/plain");
+		//resp.getWriter().println("City: "+email+" your callsign is "+ password);
+		resp.sendRedirect("/entityTest?test=FlightManager");
+	}
+}
---
+++ war/FlightManagerTest.jsp
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<p>This is a test.</p>
+
+<form action = "/flightmanagerTest" method="post">
+<H1>LOGIN </h1>
+<p>Email: <input type="text" name="emailAddr"> </p>
+<p>Password: <input type="text" name="password"> </p>
+<input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
