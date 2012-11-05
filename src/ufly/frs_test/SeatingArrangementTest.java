f6e6a0acc3422ccca550edf06d443ac91a0cdf97
Hanks <Hanks@206.87.131.30>
2012-11-04 20:21:26 -0800

    Forgot to add SeatingArrangementTest.java

---
+++ src/ufly/frs_test/SeatingArrangementTest.java
@@ -0,0 +1,47 @@
+package ufly.frs_test;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ufly.entities.Airport;
+import ufly.entities.PMF;
+import ufly.entities.SeatingArrangement.AircraftModel;
+import ufly.entities.User;
+import ufly.entities.FlightManager;
+import ufly.entities.Seat;
+import ufly.entities.SeatingArrangement;
+
+@SuppressWarnings("serial")
+public class SeatingArrangementTest extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		req.getRequestDispatcher("SeatingArrangementTest.jsp").forward(req, resp);
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException,ServletException
+	{
+		String aircraftmodel = req.getParameter("aircraftmodel");
+		SeatingArrangement newSeatingArrangement = new SeatingArrangement(aircraftmodel); 
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		try {
+            pm.makePersistent(newSeatingArrangement);
+            
+        } finally {
+            pm.close();
+        }
+		
+		//resp.setContentType("text/plain");
+		//resp.getWriter().println("Airplane: "+aircraftmodel);
+		resp.sendRedirect("/entityTest?test=SeatingArrangement");
+	}
+}
