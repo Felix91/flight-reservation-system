04afac5b68f03d3cba6d7c711d16f81aebaed8a8
Joel Vandergriendt <vandergriendtjoel@gmail.com>
2012-11-07 09:34:55 -0800

    fixed signup

--- src/ufly/frs/SignedUp.java
+++ src/ufly/frs/SignedUp.java
@@ -1,21 +1,14 @@
 package ufly.frs;
 
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
-
-import javax.jdo.PersistenceManager;
-import javax.jdo.Query;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import ufly.entities.PMF;
-import ufly.entities.User;
+import ufly.entities.*;
 
 @SuppressWarnings("serial")
-public class SignedUp extends HttpServlet {
+public class SignedUp extends UflyServlet {
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 		throws IOException,ServletException
@@ -35,35 +28,8 @@ public class SignedUp extends HttpServlet {
 			.forward(req,resp);
 			return;
 		}
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello "+firstName+" your password is "+ newPw);
-		
-		PersistenceManager pm = PMF.get().getPersistenceManager();
-		
-		User newUser = new User(emailAddr, newPw); // TODO: create Customer once User has proven to work
+		User newUser = new Customer(emailAddr, newPw,firstName,lastName); 
 		newUser.login(req.getSession());
-		try {
-            pm.makePersistent(newUser);
-            
-            // Test query
-            Query q = pm.newQuery(User.class);
-            /**
-             * in order to check this we need to check every element to see if it 
-             * is of type User, too much work, plus we should not get any
-             * other type. We just suppress the warning
-             */
-            @SuppressWarnings("unchecked")
-			List<User> results = (List<User>) q.execute();
-            Iterator<User> it = results.iterator();
-            // Print all Users in the datastore
-            resp.getWriter().println("Users registered: ");
-            while(it.hasNext())
-            {
-            	resp.getWriter().println(it.next().getEmailAddr());
-            }
-        } finally {
-            pm.close();
-        }
-		resp.sendRedirect("/search");
+		resp.sendRedirect("/");
 	}
 }
