137e4e5db35ea36c518e02130334b92e393a08cb
hasonton <hasonton@hotmail.com>
2012-11-17 20:52:46 -0800

    @Felix91: Made FlightBooking detachable.

--- src/ufly/entities/FlightBooking.java
+++ src/ufly/entities/FlightBooking.java
@@ -14,7 +14,7 @@ import javax.jdo.annotations.PrimaryKey;
 
 import com.google.appengine.api.datastore.Key;
 
-@PersistenceCapable
+@PersistenceCapable (detachable="true")
 public class FlightBooking extends SuperEntity {
 
 	/*------------ CONSTRUCTORS ------------*/
